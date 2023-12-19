3c530136127c0e7181922ba9aea9dbe7cbe469fd
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-19 12:14:44 -0700

    concept of boxing code with array list

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("cena");
+            list.Add(DateTime.Today);
+
+            var anotherList = new List<int>();
+            //anotherList.Add();
+            //    names.Add();
+
+            
+        }
+    }
+}
